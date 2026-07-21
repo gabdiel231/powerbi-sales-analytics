e0dea92f084714504783f2dff207199b71919e29
gabdiel231 <gabdiel.231@gmail.com>
2026-07-21 00:38:36 -0500

    Initialize main.py with imports and print statement

---
+++ python/main.py
@@ -0,0 +1,7 @@
+from generate_customers import *
+from generate_products import *
+from generate_sellers import *
+from generate_calendar import *
+from generate_sales import *
+
+print("Proyecto generado correctamente.")
